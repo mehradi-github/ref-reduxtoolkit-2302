37807b393982ba7ca14dacace71ef744863e639a
Mehradi <mehradi.info@gmail.com>
2024-05-13 21:20:56 +0200

    custom useTask hook for async requests

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,13 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
+import { useMemo } from "react";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useTask = () => {
+  const tasks = useAppSelector((state) => state.tasks.entities);
+  const loading = useAppSelector((state) => !!state.tasks.loading);
+  return useMemo(() => [tasks, loading] as const, [tasks, loading]);
+};
