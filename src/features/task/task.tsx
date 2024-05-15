3d7f12f216635dbad5a57790b2778b2493dd052d
Mehradi <mehradi.info@gmail.com>
2024-05-15 16:45:03 +0200

    Adding the calsses of styles via Tailwind CSS

--- src/features/task/task.tsx
+++ src/features/task/task.tsx
@@ -1,7 +1,25 @@
+import { useAppDispatch } from "../../app/hooks";
+import { removeTask } from "./task-slice";
+
 interface TaskProps {
   task: Task;
 }
 const Task = ({ task }: TaskProps) => {
-  return <div>Task</div>;
+  const dispatch = useAppDispatch();
+  return (
+    <article className="task">
+      <header className="task-header">
+        <h2 className="task-title">{task.title}</h2>
+        <button
+          className="destructive small"
+          onClick={() => {
+            dispatch(removeTask(task.id));
+          }}
+        >
+          Remove
+        </button>
+      </header>
+    </article>
+  );
 };
 export default Task;
